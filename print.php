e22b434b9a50abd0b31b5d0ab24f4799e672e1c8
Yu-Tzu Lin <linyutsu23@gmail.com>
2015-08-27 03:08:47 +0800

    complete display

--- print.php
+++ print.php
@@ -110,10 +110,13 @@ if(isset($_POST['inputName'])){
 }
 ?>
 
-<?php 
-	
-
-
+<?php 	
+	$full = array("專任", "兼任"); 
+	$title = array("教授", "副教授", "助理教授", "專案教師", "講師"); 
+	$pro = array("一般講授課程", "講座課程(邀請講員演講5場以上)", "特殊課程(如學生以外籍生居多、電腦操作、戶外課程多(例如出海)等", "語言課程", "服務學習課程"); 
+	$with = array("否", "是"); 
+	$week = array("日", "一", "二", "三", "四", "五", "六"); 
+	$TA = array("點名", "借用器材及設定軟硬體", "影印講義、考卷或其他資料", "協助作業或考卷批改", "翻譯", "講者接洽", "帶領團體討論", "戶外教學帶隊", "教學PPT製作"); 
 ?>
 	<div class="print-table">
 		<h3>國立東華大學通識教育中心<br />
@@ -128,11 +131,11 @@ if(isset($_POST['inputName'])){
 				<td class="text-center">任職系所</td>
 				<td colspan="7"><?php echo $row['dept']; ?></td>
 				<td colspan="2" class="text-center">專/兼任</td>
-				<td colspan="2"><?php echo $row['fullOrParttime']; ?></td>
+				<td colspan="2"><?php echo $full[($row['fullOrParttime']-1)]; ?></td>
 			</tr>
 			<tr>
 				<td class="text-center">教師職稱</td>
-				<td colspan="11"><?php echo $row['titles']; ?></td>
+				<td colspan="11"><?php echo $title[($row['titles']+1)]; ?></td>
 			</tr>
 			<tr>
 				<td class="text-center">手機</td>
@@ -158,27 +161,59 @@ if(isset($_POST['inputName'])){
 				<td rowspan="2" class="text-center need-hour">預期每週工作時數</td>
 				<td rowspan="2" colspan="2"><?php echo $row['TAhours'] . "小時"; ?></td>
 				<td rowspan="2" colspan="2" class="text-center">時段</td>
-				<td rowspan="2" colspan="5"><?php echo $row['TAworkday']; echo $row['TAworktime']; ?></td>
+				<td rowspan="2" colspan="5">
+					<?php 
+					for($i=0; $i<count($week); $i++)
+						if($row['TAworkday'][$i]=='Y')
+							echo "星期" . $week[$i] . "、"; 
+					echo "<br />"; 
+					if($row['TAworktime'][0]=='Y')
+						echo $i+8 . ":00 ~ "; 
+					for($i=1; $i<strlen($row['TAworktime']); $i++){
+						if($row['TAworktime'][$i]=='Y' && $row['TAworktime'][($i-1)]=='N')
+							echo $i+8 . ":00 ~ "; 
+						else if($row['TAworktime'][$i]=='N' && $row['TAworktime'][($i-1)]=='Y')
+							echo $i+8 . ":00、";
+					}
+					if($row['TAworktime'][strlen($row['TAworktime'])-1]=='Y')
+						echo strlen($row['TAworktime'])-1+9 . ":00"; 
+					?>
+				</td>
 			</tr>
 			<tr>
 				<td class="text-center">跟課與否</td>
-				<td><?php echo $row['TAwithClass']; ?></td>
+				<td><?php echo $with[$row['TAwithClass']]; ?></td>
 			</tr>
 			<tr class="top bottom">
 				<td class="text-center">課程屬性</td>
-				<td colspan="11"><?php echo $row['courseProperty']; ?></td>
+				<td colspan="11"><?php echo $pro[($row['courseProperty']-1)]; ?></td>
 			</tr>
 			<tr class="top">
 				<td class="text-center">申請說明（含TA需協助事項）</td>
-				<td colspan="5"><?php echo $row['TAcontent']; ?></td>
-				<td colspan="6"><?php echo $row['TAcontent']; ?></td>
+				<td colspan="5">
+					<?php
+						for($i=0; $i<count($TA); $i++){
+							if($row['TAcontent'][$i]=='Y')
+								echo "■ "; 
+							else 
+								echo "□ "; 
+							echo $TA[$i] . "<br />"; 
+						}
+					?>
+				</td>
+				<td colspan="6" style="vertical-align: top; ">
+					其他說明：<br />	
+				<?php 
+					for($i=10; $i<strlen($row['TAcontent']); $i++)
+						echo $row['TAcontent'][$i]; 
+				?></td>
 			</tr>
 			<tr>
-				<td class="text-center need-apply">申請人簽名</td>
+				<td class="text-center need-apply">申請人<br />簽名</td>
 				<td colspan="3" class="need-sign"></td>
-				<td class="text-center need-gen">通識中心主任</td>
+				<td class="text-center need-gen">通識中心<br />主任</td>
 				<td colspan="5" class="need-sign"></td>
-				<td class="text-center need-ce">共同教育委員會主委</td>
+				<td class="text-center need-ce">共同教育<br />委員會<br />主委</td>
 				<td class="need-sign"></td>
 			</tr>
 		</table>
